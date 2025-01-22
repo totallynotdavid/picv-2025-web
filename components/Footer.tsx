e39494d6594e553eef583138206d0e24b7c86de8
David Duran <dadch1404@gmail.com>
2025-01-22 12:03:58 -0500

    delete padding-top

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { V0Logo } from './v0logo';
 
 const Footer = () => (
   <footer>
-    <div className="custom-screen pt-16">
+    <div className="custom-screen">
       <div className="mt-10 py-10 border-t items-center justify-between flex">
         <p className="text-gray-600">
           Creado en el{' '}
